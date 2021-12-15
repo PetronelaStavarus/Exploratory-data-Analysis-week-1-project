50864e11ae19a571784312c1707a412ae6a65678
PetronelaStavarus <91903004+PetronelaStavarus@users.noreply.github.com>
2021-12-15 22:22:01 +0200

    Add files via upload

---
+++ Plot3.R
@@ -0,0 +1,11 @@
+data=read.table("household_power_consumption.txt",header=TRUE, sep=";",na.strings = "?",stringsAsFactors = FALSE)
+mydata=subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
+mydata$Time=strptime(mydata$Time,format="%H:%M:%S")
+mydata$Date=as.Date(mydata$Date, format= "%d/%m/%Y")
+plot(mydata$Time,mydata$Sub_metering_1,type="l", ylab="Energy Sub metering")
+lines(mydata$Time,mydata$Sub_metering_2,col="red")
+lines(mydata$Time,mydata$Sub_metering_3,col="blue")
+legend("topright", col=c("black", "red","blue"), lty=1,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.copy(png,filename="plot3.png"))
+dev.off()
+
